499ca0dcfed147080b3d5093d025d6db5fc8914f
nguyenvukhang <brew4k@gmail.com>
2022-02-09 08:40:04 +0800

    tmux: fixed uni script

--- tmux/uni.sh
+++ tmux/uni.sh
@@ -4,7 +4,7 @@
 START=$REPOS/uni
 
 # start tmux session (detached)
-tmux new-session -As "uni" -c "$START"
+tmux new-session -As "uni" -c "$START" -d
 
 # switch to tmux session
 tmux switch-client -t "uni"
