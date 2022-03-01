fa1df5efe9227b0de105c6d3209d0e1885806fb5
nguyenvukhang <brew4k@gmail.com>
2022-03-01 08:06:15 +0800

    yabai: updates

--- mac/.config/yabai/spaces.py
+++ mac/.config/yabai/spaces.py
@@ -5,18 +5,38 @@ import json
 import subprocess
 import os
 
-yabai_cmd = ['yabai', '-m', 'query', '--spaces']
+argv = sys.argv
 
-raw_output = subprocess.run(yabai_cmd, stdout=subprocess.PIPE)
-human_readable = raw_output.stdout.decode('utf-8')
-space_info = json.loads(human_readable)
-displays = len(set(map(lambda x: x['display'], space_info)))
-print(displays)
-spaces = len(space_info)
+def get_json(str):
+    term_cmd = str.split()
+    raw_output = subprocess.run(term_cmd, stdout=subprocess.PIPE)
+    human_readable = raw_output.stdout.decode('utf-8')
+    return json.loads(human_readable)
 
-if displays == 1:
-    os.system('yabai -m space --focus 2')
-elif displays == 2:
-    os.system('yabai -m space --focus 3')
-else:
-    print('you made it to three display, damn!')
+spaces = get_json('yabai -m query --spaces')
+displays = get_json('yabai -m query --displays')
+current_space = get_json('yabai -m query --spaces --space')
+current_display = current_space['display']
+
+dwm = {x+1: [] for x in range(len(displays))}
+
+for i in spaces:
+    dwm[i['display']].append(i['index'])
+
+print(dwm, current_display)
+
+def get_destination(first_arg, current_display):
+    n = int(first_arg)
+    local_spaces = dwm[current_display]
+    local_first = local_spaces[0]
+    if n <= len(local_spaces):
+        return local_spaces[n-local_first]
+    else:
+        return 0
+
+if len(argv) > 1:
+    first_arg = argv[1]
+    target = get_destination(first_arg, current_display)
+    print('target', target)
+    if target != 0:
+        os.system('yabai -m space --focus %s' % (target))
