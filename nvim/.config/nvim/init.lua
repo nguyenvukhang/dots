079f380e59edd42dc5c826d26455c82cd321c6c7
nguyenvukhang <brew4k@gmail.com>
2022-03-26 09:44:29 +0800

    stable

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,6 +3,7 @@ local source = function(e) vim.cmd('source '..conf..'/'..e) end
 
 source('vim/auto-pairs.vim')
 source('vim/plugged-in.vim') -- plugins
+source('vim/statusline.vim') -- statusline
 require('brew.sets')         -- sets
 source('vim/remaps.vim')     -- remaps
 
