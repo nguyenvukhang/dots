d333ddb263e8900ff75343fb54076b4bba096e09
nguyenvukhang <brew4k@gmail.com>
2022-03-27 09:32:28 +0800

    shortened padding

--- nvim/.config/nvim/lua/lualine/component.lua
+++ nvim/.config/nvim/lua/lualine/component.lua
@@ -48,27 +48,20 @@ end
 ---adds spaces to left and right of a component
 function M:apply_padding()
   local padding = self.options.padding
-  local l_padding, r_padding
   if padding == nil then
     padding = 1
   end
-  if type(padding) == 'number' then
-    l_padding, r_padding = padding, padding
-  elseif type(padding) == 'table' then
-    l_padding, r_padding = padding.left, padding.right
-  end
-  if l_padding then
+
+  if padding then
     if self.status:find('%%#.*#') == 1 then
       -- When component has changed the highlight at begining
       -- we will add the padding after the highlight
       local pre_highlight = vim.fn.matchlist(self.status, [[\(%#.\{-\}#\)]])[2]
-      self.status = pre_highlight .. string.rep(' ', l_padding) .. self.status:sub(#pre_highlight + 1, #self.status)
+      self.status = pre_highlight .. string.rep(' ', padding) .. self.status:sub(#pre_highlight + 1, #self.status)
     else
-      self.status = string.rep(' ', l_padding) .. self.status
+      self.status = string.rep(' ', padding) .. self.status
     end
-  end
-  if r_padding then
-    self.status = self.status .. string.rep(' ', r_padding)
+    self.status = self.status .. string.rep(' ', padding)
   end
 end
 
