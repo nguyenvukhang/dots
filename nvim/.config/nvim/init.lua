e2599ed7e8fc2066062316861580240b4f4d8391
nguyenvukhang <brew4k@gmail.com>
2022-03-27 16:42:20 +0800

    nvim: add startuptimes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,11 +1,15 @@
 local conf = require('brew.core').env.conf
 local source = function(e) vim.cmd('source '..conf..'/'..e) end
 
-source('vim/auto-pairs.vim')
-source('vim/plugged-in.vim') -- plugins
-require('brew.sets')         -- sets
-source('vim/remaps.vim')     -- remaps
+-- the heaviest section of the config
+source('vim/plugged-in.vim')
+require('brew.sets')
+require('brew.telescope')
+require('brew.lsp')
 
+-- the ligher half
+source('vim/auto-pairs.vim')
+source('vim/remaps.vim')
 require('brew.plugins.markdown_preview')
 require('brew.plugins.lualine')
 require('brew.plugins.nvim_cmp')
@@ -13,8 +17,4 @@ require('brew.plugins.nvim_comment')
 require('brew.plugins.prettier')
 require('brew.plugins.ultisnips')
 require('brew.plugins.vimtex')
-
-require('brew.telescope')
-require('brew.lsp')
-
-source('vim/augroup.vim')    -- autocmds
+source('vim/augroup.vim')
