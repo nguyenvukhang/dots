9b193541028d43db51d2f87b9b2e45b12591af80
nguyenvukhang <brew4k@gmail.com>
2022-03-29 19:23:40 +0800

    nvim: fix telescope

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,12 +4,13 @@ local source = function(e) vim.cmd('source '..conf..'/'..e) end
 -- the heaviest section of the config
 source('vim/plugged-in.vim')
 require('brew.sets')
+source('vim/auto-pairs.vim')
+source('vim/remaps.vim')
+
 require('brew.telescope')
 require('brew.lsp')
 
--- the ligher half
-source('vim/auto-pairs.vim')
-source('vim/remaps.vim')
+-- the lighter half
 require('brew.plugins.markdown_preview')
 require('brew.plugins.lualine')
 require('brew.plugins.nvim_cmp')
