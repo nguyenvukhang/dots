68170ccf86daee240b6f4f3b2366a9f40dd22cf6
nguyenvukhang <brew4k@gmail.com>
2022-07-02 20:48:26 +0800

    can parse jsons now

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -23,8 +23,20 @@ require("brew.plugins.vimtex")
 source("vim/functions.vim")
 source("vim/augroup.vim")
 
-local open = io.open
-local file = open("/Users/khang/modtree/hours.json", "rb")
-local json = file:read("*a")
-print(json)
-file:close()
+-- os.execute("node $HOME/modtree/hours.js")
+local json = require("packages.json")
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	callback = function()
+		local open = io.open
+		local file = open(env.home .. '/modtree/wiki/khang_hours.json', "rb")
+    local jsonString = file:read "*a"
+    local data = json.decode(jsonString)
+		print(vim.inspect(data))
+		-- local buffer = vim.fn.getbufinfo()
+		-- for k, v in ipairs(buffer) do
+			-- print("yes", k, v)
+		-- end
+		-- os.execute("node $HOME/modtree/hours.js " .. )
+	end,
+})
