f7abe8afacba288d2d95d0ecddbf917582d86e2e
nguyenvukhang <brew4k@gmail.com>
2022-07-02 22:10:11 +0800

    add support for close

--- nvim/.config/nvim/lua/modtree.lua
+++ nvim/.config/nvim/lua/modtree.lua
@@ -28,33 +28,47 @@ end
 
 vim.api.nvim_create_augroup("modtree", { clear = true })
 
-local modtree_data_filename = 'khang.json'
+local modtree_data_filename = "khang.json"
 local modtree_data_filepath = env.home .. "/modtree/wiki/hours/" .. modtree_data_filename
+
+--
+-- takes in one param: action, which is either "open" or "close"
+-- and writes to the modtree log
+--
+local modtree_log_action = function(action)
+	local filename = vim.fn.expand("%:p")
+	-- cancel on empty buffers
+	if filename == "" or vim.fn.bufname() == modtree_data_filename then
+		return
+	end
+	-- read existing data
+	local data = json.read(modtree_data_filepath)
+	-- get current timestamp
+	local timestamp = {
+		file = filename,
+		date = os.date("%Y/%m/%d"),
+		time = os.date("%X"),
+		action = action,
+	}
+	-- add the timestamp to current data
+	table.insert(data["timestamps"], timestamp)
+	-- write to json file
+	local result = json.write(modtree_data_filepath, data)
+	if result then
+		print("updated modtree log!")
+	end
+end
 
 vim.api.nvim_create_autocmd("BufEnter", {
 	group = "modtree",
 	callback = function()
-		local filename = vim.fn.expand("%:p")
-		-- cancel on empty buffers
-		if filename == "" or vim.fn.bufname() == modtree_data_filename then
-			return
-		end
-		-- read existing data
-		local data = json.read(modtree_data_filepath)
-		-- get current timestamp
-		local timestamp = {
-			file = filename,
-			date = os.date("%Y/%m/%d"),
-			time = os.date("%X"),
-			action = "open",
-		}
-		-- add the timestamp to current data
-		table.insert(data["timestamps"], timestamp)
-		-- write to json file
-		local result = json.write(modtree_data_filepath, data)
-		if result then
-			print("updated modtree log!")
-		end
-    print(filename)
+    modtree_log_action('open')
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufLeave", {
+	group = "modtree",
+	callback = function()
+    modtree_log_action('close')
 	end,
 })
