4b4e436b1e91e2bfaff846c2e2c4159b9d561cba
nguyenvukhang <brew4k@gmail.com>
2022-07-02 20:51:44 +0800

    create augroup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -24,18 +24,21 @@ source("vim/functions.vim")
 source("vim/augroup.vim")
 
 -- os.execute("node $HOME/modtree/hours.js")
-local json = require("packages.json")
+local json = require("json")
+
+vim.api.nvim_create_augroup("modtree", { clear = true })
 
 vim.api.nvim_create_autocmd("BufEnter", {
+	group = "modtree",
 	callback = function()
 		local open = io.open
-		local file = open(env.home .. '/modtree/wiki/khang_hours.json', "rb")
-    local jsonString = file:read "*a"
-    local data = json.decode(jsonString)
-		print(vim.inspect(data))
+		local file = open(env.home .. "/modtree/wiki/khang_hours.json", "rb")
+		local jsonString = file:read("*a")
+		local data = json.decode(jsonString)
+		-- print(vim.inspect(data))
 		-- local buffer = vim.fn.getbufinfo()
 		-- for k, v in ipairs(buffer) do
-			-- print("yes", k, v)
+		-- print("yes", k, v)
 		-- end
 		-- os.execute("node $HOME/modtree/hours.js " .. )
 	end,
