134ec74ffca88cca871916a438104b68b87dc0d6
nguyenvukhang <brew4k@gmail.com>
2022-07-26 23:46:46 +0800

    shorten lsp

--- nvim/.config/nvim/lua/brew/lsp.lua
+++ nvim/.config/nvim/lua/brew/lsp.lua
@@ -4,45 +4,40 @@
 
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local nvim_lsp = require("lspconfig")
-local set_keymap = function(bufnr, ...)
-	vim.api.nvim_buf_set_keymap(bufnr, ...)
+local function set_keymap(bufnr, lhs, callback)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", lhs, "", {
+		noremap = true,
+		silent = true,
+		callback = callback,
+	})
 end
-local opts = { noremap = true, silent = true }
 
 -- lsp-specific keymaps
-local on_attach = function(_, bufnr)
-	set_keymap(bufnr, "n", "gd", ":lua vim.lsp.buf.definition()<CR>", opts)
-	set_keymap(bufnr, "n", "gD", ":lua vim.lsp.buf.declaration()<CR>", opts)
-	set_keymap(bufnr, "n", "gt", ":lua vim.lsp.buf.type_definition()<CR>", opts)
-	set_keymap(bufnr, "n", "gr", ":lua vim.lsp.buf.references()<CR>", opts)
-	set_keymap(bufnr, "n", "gi", ":lua vim.lsp.buf.implementation()<CR>", opts)
-	set_keymap(bufnr, "n", "K", ":lua vim.lsp.buf.hover()<CR>", opts)
+local function on_attach(_, bufnr)
+	set_keymap(bufnr, "gd", vim.lsp.buf.definition)
+	set_keymap(bufnr, "gD", vim.lsp.buf.declaration)
+	set_keymap(bufnr, "gt", vim.lsp.buf.type_definition)
+	set_keymap(bufnr, "gr", vim.lsp.buf.references)
+	set_keymap(bufnr, "gi", vim.lsp.buf.implementation)
+	set_keymap(bufnr, "K", vim.lsp.buf.hover)
 	print("LSP attached!")
 end
 
 -- javascript lsp
 nvim_lsp.tsserver.setup({
-	on_attach = function(_, bufnr)
-		on_attach(_, bufnr)
-	end,
+	on_attach = on_attach,
 	capabilities = capabilities,
 })
 
 -- python lsp
 nvim_lsp.pyright.setup({
-	on_attach = function(_, bufnr)
-		on_attach(_, bufnr)
-	end,
+	on_attach = on_attach,
 	capabilities = capabilities,
 })
 
--- }
-
 -- rust lsp
 nvim_lsp.rls.setup({
-	on_attach = function(_, bufnr)
-		on_attach(_, bufnr)
-	end,
+	on_attach = on_attach,
 	capabilities = capabilities,
 	settings = {
 		rust = {
@@ -58,22 +53,14 @@ local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 nvim_lsp.sumneko_lua.setup({
+	on_attach = on_attach,
 	capabilities = capabilities,
 	settings = {
 		Lua = {
-			runtime = {
-				version = "LuaJIT",
-				path = runtime_path,
-			},
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			telemetry = {
-				enable = false,
-			},
+			runtime = { version = "LuaJIT", path = runtime_path },
+			diagnostics = { globals = { "vim" } },
+			workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+			telemetry = { enable = false },
 		},
 	},
 })
