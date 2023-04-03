ac4c583df9c04cac9ae8683f28f44c1839874061
nguyenvukhang <brew4k@gmail.com>
2023-04-03 11:12:00 +0800

    feat: expand on colors database

--- colors/types.ts
+++ colors/types.ts
@@ -1,10 +1,26 @@
-type BackgroundKind = "light" | "dark";
-type C = string;
-type TermColors = [C, C, C, C, C, C, C, C];
-type Pair = { fg: C, bg: C}
-export type ColorScheme = {
-  kind: BackgroundKind;
-  term: TermColors;
-  main: Pair
-  select: Pair
-};
+type FgBgPair = { fg: string; bg: string }
+
+type ColorScheme = {
+  kind: 'light' | 'dark'
+  /**
+   * Standard terminal colors.
+   * [red, green, yellow, blue, magenta, cyan]
+   */
+  term: [string, string, string, string, string, string]
+  main: FgBgPair
+  select: FgBgPair
+}
+
+class Themes {
+  db: Record<string, ColorScheme>
+
+  constructor() {
+    this.db = {}
+  }
+
+  add(name: string, colorscheme: ColorScheme) {
+    this.db[name] = colorscheme
+  }
+}
+
+export const themes = new Themes()
