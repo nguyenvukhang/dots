7d6ae86ecc7052ec9e3a58b3470e03efc41d370f
nguyenvukhang <brew4k@gmail.com>
2023-04-12 01:15:23 +0800

    theme: sike, back on gruvbox

---
+++ nvim/lua/gruvbox/init.lua
@@ -0,0 +1,34 @@
+local M = {}
+
+-- default configs
+M.config = {
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = '', -- can be "hard", "soft" or empty string
+  dim_inactive = false,
+}
+
+function M.setup(config)
+  M.config = vim.tbl_deep_extend('force', M.config, config or {})
+end
+
+M.load = function()
+  if vim.version().minor < 8 then
+    vim.notify_once('gruvbox.nvim: you must use neovim 0.8 or higher')
+    return
+  end
+
+  -- reset colors
+  if vim.g.colors_name then vim.cmd.hi('clear') end
+
+  vim.g.colors_name = 'gruvbox'
+  vim.o.termguicolors = true
+
+  local groups = require('gruvbox.groups').setup()
+
+  -- add highlights
+  for group, settings in pairs(groups) do
+    vim.api.nvim_set_hl(0, group, settings)
+  end
+end
+
+return M
