4700bd45fc8127a7c3b413b77ba57c7a3d64544c
nguyenvukhang <brew4k@gmail.com>
2023-04-12 17:20:14 +0800

    nvim: move entire latex transform into lua

--- nvim/lua/brew/core/autocmd.lua
+++ nvim/lua/brew/core/autocmd.lua
@@ -82,23 +82,54 @@ autocmd({ 'BufEnter', 'FocusGained', 'BufWritePost' }, {
   end,
 })
 
+--- @return boolean, string[]
+local function pdflatex(doc)
+  vim.fn.delete('.build/tmp', 'rf')
+  vim.fn.mkdir('.build', 'p')
+  local args = {
+    'pdflatex',
+    '--halt-on-error',
+    '--output-directory=.build',
+    '--jobname',
+    vim.fn.expand('%:t:r'),
+  }
+  local cmd = table.concat(args, ' ') .. '>.build/tmp;echo $?>.build/status'
+  local handle = io.popen(cmd, 'w')
+  if handle ~= nil then
+    local transform = require('brew.latex').transform
+    for _, v in pairs(doc) do
+      handle:write(transform(v) .. '\n')
+    end
+    handle:flush()
+    handle:close()
+    local ok = vim.fn.readfile('.build/status')[1] == '0'
+    local output = ok and {} or vim.fn.readfile('.build/tmp')
+    return ok, output
+  end
+  return false, {}
+end
+
 autocmd({ 'BufWritePost' }, {
   pattern = { '*.tex' },
   callback = function()
+    local buffer = vim.fn.getline(1, '$')
+    local success, output = pdflatex(buffer)
     vim.cmd('redraw')
-    -- local output = vim.fn.systemlist('latext ' .. vim.fn.expand('%:p'))
-    vim.fn.systemlist('make build')
-    -- local ok = #output == 0
-    -- local msg = ok and 'successful build!' or 'build failed.'
-    -- print('[pdflatex] ' .. msg)
-    -- if ok then return end
-    -- local qf = {}
-    -- for _, v in pairs(output) do
-    --   table.insert(qf, { text = v })
-    -- end
-    -- vim.fn.setqflist(qf)
+    if not success then
+      print('[pdflatex] build failed.')
+      local qf = {}
+      for _, v in pairs(output) do
+        table.insert(qf, { text = v })
+      end
+      vim.fn.setqflist(qf)
+    else
+      print('[pdflatex] build successful!')
+    end
   end,
 })
+
+-- temporary for writing math
+-- autocmd({ 'BufWritePost' }, { pattern = { '*.tex.lua' }, callback = function() vim.cmd('redraw') vim.fn.systemlist('make build') end, })
 
 autocmd(general_entry, {
   pattern = { '*.prez' },
