11ddaa37992d721d19c1f93bedbaecc4e0c97358
nguyenvukhang <brew4k@gmail.com>
2023-04-20 00:47:30 +0800

    nvim: remove telescope util

--- nvim/lua/brew/telescope/remaps.lua
+++ nvim/lua/brew/telescope/remaps.lua
@@ -1,41 +1,58 @@
 local builtin = require('telescope.builtin')
 local nnoremap = require('brew.core').nnoremap
-local util = require('brew.telescope.util')
 local git_workspace_root = require('brew.core').git_workspace_root
 local search = { files = {}, string = {} }
-local t = require('telescope.builtin')
+local telescope = require('telescope.builtin')
+
+-- stylua: ignore
+local t = { file_ignore_patterns = {
+  'node_modules', 'LICENSE', 'autoload/plug.vim', '.ccls-cache',
+  'yarn.lock', 'error.log', '%.git/', '%.gpg', '%.DS_Store', '%.pdf',
+  '%.pdf', '%.png', '%.jpg', '%.gif', '%.sql',
+}}
 
 -- git repo operations
 search.files.repo = function()
   if not git_workspace_root() then return end
-  util.search.git_files('Files in Repo')
+  t.prompt_title, t.cwd, t.search = 'Files in Repo', nil, nil
+  telescope.git_files(t)
 end
 search.string.repo = function()
   local repo_dir = git_workspace_root()
   if repo_dir == nil then return end
-  local q = vim.fn.input('Search For > ')
-  if q == '' then return end
-  util.search.string('Word In Repo', repo_dir, q)
+  local query = vim.fn.input('Search For > ')
+  if query == '' then return end
+  t.prompt_title, t.cwd, t.search = 'Word in Repo', repo_dir, query
+  telescope.grep_string(t)
 end
 
 -- current working dir operations
-search.files.cwd = function() util.search.files('Files in CWD') end
+search.files.cwd = function()
+  t.prompt_title, t.cwd, t.search = 'Files in CWD', nil, nil
+  telescope.find_files(t)
+end
 search.string.cwd = function()
   local cwd = vim.fn.getcwd():gsub(vim.env.HOME, '~')
-  local q = vim.fn.input('Search In [' .. cwd .. '] > ')
-  if q == '' then return end
-  util.search.string('Word In CWD', cwd, q)
+  local query = vim.fn.input('Search In [' .. cwd .. '] > ')
+  if query == '' then return end
+  t.prompt_title, t.cwd, t.search = 'Word in CWD', cwd, query
+  telescope.grep_string(t)
 end
 
 search.string.cursor = function()
   local repo_dir = git_workspace_root()
   if repo_dir == nil then return end
-  util.search.string('Word in Repo', repo_dir)
+  t.prompt_title, t.cwd, t.search = 'Word in Repo', repo_dir, nil
+  telescope.grep_string(t)
 end
 
-search.files.dots = function() util.search.files('dotfiles', vim.env.DOTS) end
+search.files.dots = function()
+  t.prompt_title, t.cwd, t.search = 'dotfiles', vim.env.DOTS, nil
+  telescope.find_files(t)
+end
 search.files.university = function()
-  util.search.files('university', vim.env.UNI)
+  t.prompt_title, t.cwd, t.search = 'university', vim.env.UNI, nil
+  telescope.find_files(t)
 end
 
 -- remaps!
