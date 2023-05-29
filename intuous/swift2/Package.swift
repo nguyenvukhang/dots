36378266d369c05b007aa3f7e80e4098d41193dd
nguyenvukhang <brew4k@gmail.com>
2023-05-29 10:19:08 +0800

    drop unused

--- intuous/swift2/Package.swift
+++
@@ -1,6 +0,0 @@
-// swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
-import PackageDescription
-
-let package = Package(name: "swift2", targets: [.executableTarget(name: "swift2", path: ".")])
