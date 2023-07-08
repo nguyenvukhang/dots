803eaaf89854aa5a777718f87a67e829b8046b6c
khang <khang@mac-4.local>
2023-07-09 00:34:14 +0800

    drop a bunch

--- fs/.archive/scripts/arch/spotify.sh
+++
@@ -1,8 +0,0 @@
-running=$(pidof spotify)
-if [ "$running" != "" ]; then
-    artist=$(playerctl -p "spotify" metadata artist)
-    song=$(playerctl -p "spotify" metadata title | cut -c 1-60)
-    if [ "$song" != "" ]; then
-        echo -n "$artist <fc=#888888>・</fc> $song <fc=#666666> |  </fc>"
-    fi
-fi
