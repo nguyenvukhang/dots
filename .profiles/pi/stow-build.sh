96fdffabbb586ddc96b7c025d26cf60da7714f31
nguyenvukhang <brew4k@gmail.com>
2023-07-09 10:58:01 +0800

    build profile for pi

---
+++ .profiles/pi/stow-build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ln -sf ../../../../nvim ./home/.config/nvim
+ln -sf ../../../../tmux ./home/.config/tmux
+ln -sf ../../../../zsh ./home/.config/zsh
+ln -sf ../../../../@/alacritty ./home/.config/alacritty
+ln -sf ../../../../@/git ./home/.config/git
+ln -sf ../../../../zsh/zshenv/pi ./root/etc//zsh/zshenv
