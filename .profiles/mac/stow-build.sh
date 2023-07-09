43d3fdb50f3b234cddd46a09d8677e3fddb15ae2
nguyenvukhang <brew4k@gmail.com>
2023-07-09 10:56:08 +0800

    link zshenv in etc

--- .profiles/mac/stow-build.sh
+++ .profiles/mac/stow-build.sh
@@ -5,3 +5,4 @@ ln -sf ../../../../tmux ./home/.config/tmux
 ln -sf ../../../../zsh ./home/.config/zsh
 ln -sf ../../../../@/alacritty ./home/.config/alacritty
 ln -sf ../../../../@/git ./home/.config/git
+ln -sf ../../../../zsh/zshenv/mac ./root/etc/zshenv
