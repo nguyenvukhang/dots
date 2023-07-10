a4050ffe2fd6e5e2743a249f846895cb7e546042
nguyenvukhang <brew4k@gmail.com>
2023-07-10 13:11:37 +0800

    start profiles

--- .profiles/mac/stow-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ln -sf ../../../../nvim ./home/.config/nvim
-ln -sf ../../../../tmux ./home/.config/tmux
-ln -sf ../../../../zsh ./home/.config/zsh
-ln -sf ../../../../@/alacritty ./home/.config/alacritty
-ln -sf ../../../../@/git ./home/.config/git
-ln -sf ../../../../zsh/zshenv/mac ./root/etc/zshenv
