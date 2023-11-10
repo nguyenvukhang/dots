6b142475a111cc047d982cf2f89dfc0f3117c1b6
nguyenvukhang <brew4k@gmail.com>
2023-11-10 16:47:01 -0500

    colorize tab bar

--- @/wezterm/wezterm.lua
+++ @/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local w = require("wezterm")
 local act = w.action
+local wezterm = w
 
 local config = {}
 if w.config_builder then
@@ -11,6 +12,18 @@ config.font_size = 14
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.font = w.font("JetBrains Mono")
 config.cell_width = 0.9
+
+-- gruvbox!
+local g = {
+	black = "#282828",
+	red = "#ea6962",
+	green = "#a9b665",
+	yellow = "#d8a657",
+	blue = "#7daea3",
+	magenta = "#d3869b",
+	cyan = "#89b48c",
+	gray = "#a89984",
+}
 
 config.colors = {
 	foreground = "#ebdbb2",
@@ -38,6 +51,27 @@ config.colors = {
 	quick_select_label_fg = { Color = "#ffffff" },
 	quick_select_match_bg = { AnsiColor = "Navy" },
 	quick_select_match_fg = { Color = "#ffffff" },
+	tab_bar = {
+		background = "#181818",
+		active_tab = {
+			bg_color = "#282828",
+			fg_color = "#a9b665",
+			intensity = "Normal",
+			-- "None" | "Single" | "Double"
+			underline = "None",
+			italic = false,
+			strikethrough = false,
+		},
+		inactive_tab = {
+			bg_color = "#1b1032",
+			fg_color = "#808080",
+		},
+		inactive_tab_hover = {
+			bg_color = "#3b3052",
+			fg_color = "#909090",
+			italic = true,
+		},
+	},
 }
 
 local function mk_key(k, m, a)
@@ -64,6 +98,28 @@ config.keys = {
 			end),
 		})
 	),
+}
+
+config.window_frame = {
+	-- The font used in the tab bar.
+	-- Roboto Bold is the default; this font is bundled
+	-- with wezterm.
+	-- Whatever font is selected here, it will have the
+	-- main font setting appended to it to pick up any
+	-- fallback fonts you may have used there.
+	font = wezterm.font({ family = "Helvetica", weight = "Bold" }),
+
+	-- The size of the font in the tab bar.
+	-- Default to 10.0 on Windows but 12.0 on other systems
+	font_size = 12.0,
+
+	-- The overall background color of the tab bar when
+	-- the window is focused
+	active_titlebar_bg = "#333333",
+
+	-- The overall background color of the tab bar when
+	-- the window is not focused
+	inactive_titlebar_bg = "#ff0000",
 }
 
 return config
