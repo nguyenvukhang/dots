dacc593f7f36771a0d4f43050d08804c6f76a9b7
nguyenvukhang <brew4k@gmail.com>
2023-11-10 16:16:22 -0500

    wezterm: start config properly

---
+++ @/wezterm/wezterm.lua
@@ -0,0 +1,61 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.use_ime = false
+
+config.colors = {
+	foreground = "silver",
+	background = "black",
+
+	cursor_bg = "#bdae93",
+	cursor_fg = "black",
+	cursor_border = "#bdae93",
+	selection_fg = "black",
+	selection_bg = "#ebdbb2",
+	scrollbar_thumb = "#222222",
+	split = "#a9b665",
+
+	ansi = {
+		"#282828",
+		"#ea6962",
+		"#a9b665",
+		"#d8a657",
+		"#7daea3",
+		"#d3869b",
+		"#89b48c",
+		"#a89984",
+	},
+	brights = {
+		"#928374",
+		"#ea6962",
+		"#a9b665",
+		"#d8a657",
+		"#7daea3",
+		"#d3869b",
+		"#89b48c",
+		"#ebdbb2",
+	},
+
+	compose_cursor = "orange",
+
+	copy_mode_active_highlight_bg = { Color = "#000000" },
+	copy_mode_active_highlight_fg = { AnsiColor = "Black" },
+	copy_mode_inactive_highlight_bg = { Color = "#52ad70" },
+	copy_mode_inactive_highlight_fg = { AnsiColor = "White" },
+
+	quick_select_label_bg = { Color = "peru" },
+	quick_select_label_fg = { Color = "#ffffff" },
+	quick_select_match_bg = { AnsiColor = "Navy" },
+	quick_select_match_fg = { Color = "#ffffff" },
+}
+
+return config
