a769177318ff36e2c36f149b6729979711574ff5
nguyenvukhang <brew4k@gmail.com>
2023-11-16 09:16:05 -0500

    hmm

--- @/kitty/tab_bar.py
+++ @/kitty/tab_bar.py
@@ -2,7 +2,7 @@
 
 import datetime, json
 from kitty.fast_data_types import get_options
-from kitty.tab_bar import as_rgb, draw_tab_with_separator
+from kitty.tab_bar import as_rgb, draw_title
 from kitty.utils import color_as_int
 
 OPTS = get_options()
@@ -17,13 +17,16 @@ def birthday():
 
     with open(path.join(environ["DOTS"], "personal/birthdays/db.json"), "r") as f:
         p = json.load(f).get(NOW.strftime("%d-%b"), None)
-        # return 
+        # return
         # return str(j)
         # return str(p)
         return "↑" if not p else f"{', '.join(p)} lvl up ↑"
 
 
-def _draw_right_status(screen):
+def _draw_right_status(sc, is_last):
+    if not is_last:
+        return sc.cursor.x
+
     cells = [
         (BDAE_FG, 0, birthday()),
         (DATE_FG, 0, NOW.strftime("  %d %b ")),
@@ -31,23 +34,46 @@ def _draw_right_status(screen):
     ]
 
     right_status_length = sum([len(x) for _, _, x in cells])
-    draw_spaces = screen.columns - screen.cursor.x - right_status_length
+    draw_spaces = sc.columns - sc.cursor.x - right_status_length
 
     if draw_spaces > 0:
-        screen.draw(" " * draw_spaces)
+        sc.draw(" " * draw_spaces)
 
     for fg, bg, cell in cells:
-        screen.cursor.fg, screen.cursor.bg = fg, bg
-        screen.draw(cell)
+        sc.cursor.fg, sc.cursor.bg = fg, bg
+        sc.draw(cell)
 
-    screen.cursor.fg, screen.cursor.bg = 0, 0
-    screen.cursor.x = max(screen.cursor.x, screen.columns - right_status_length)
-    return screen.cursor.x
+    sc.cursor.fg, sc.cursor.bg = 0, 0
+    sc.cursor.x = max(sc.cursor.x, sc.columns - right_status_length)
+    return sc.cursor.x
 
 
-def draw_tab(dd, sc, tab, b, mtl, idx, is_last, exd):
-    if is_last:
-        _draw_right_status(sc)
-        return sc.cursor.x
-    else:
-        return draw_tab_with_separator(dd, sc, tab, b, mtl, idx, is_last, exd)
+def _draw_left_status(dd, sc, tab, b, mtl, idx, is_last) -> int:
+    if dd.leading_spaces:
+        sc.draw(" " * dd.leading_spaces)
+
+    # draw tab title
+    draw_title(dd, sc, tab, idx)
+
+    trailing_spaces = min(max_title_length - 1, dd.trailing_spaces)
+    max_title_length -= trailing_spaces
+    extra = sc.cursor.x - b - max_title_length
+    if extra > 0:
+        sc.cursor.x -= extra + 1
+        sc.draw("…")
+    if trailing_spaces:
+        sc.draw(" " * trailing_spaces)
+
+    sc.cursor.bold = sc.cursor.italic = False
+    sc.cursor.fg = 0
+    if not is_last:
+        sc.cursor.bg = as_rgb(color_as_int(dd.inactive_bg))
+        sc.draw(dd.sep)
+    sc.cursor.bg = 0
+    return sc.cursor.x
+
+
+def draw_tab(dd, sc, tab, b, mtl, idx, is_last, _):
+    end = _draw_left_status(dd, sc, tab, b, mtl, idx, is_last)
+    _draw_right_status(sc, is_last)
+    return end
