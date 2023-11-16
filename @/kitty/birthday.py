69244d57403c6e6427ce039a777147ce4f7d01aa
nguyenvukhang <brew4k@gmail.com>
2023-11-16 09:17:04 -0500

    good

--- @/kitty/birthday.py
+++ @/kitty/birthday.py
@@ -1,9 +1,6 @@
-"""draw kitty tab"""
 # pyright: reportMissingImports=false
-# pylint: disable=E0401,C0116,C0103,W0603,R0913
 
 import datetime, json
-
 from kitty.fast_data_types import get_options
 from kitty.tab_bar import as_rgb, draw_title
 from kitty.utils import color_as_int
@@ -20,19 +17,47 @@ def birthday():
 
     with open(path.join(environ["DOTS"], "personal/birthdays/db.json"), "r") as f:
         p = json.load(f).get(NOW.strftime("%d-%b"), None)
+        # return
+        # return str(j)
+        # return str(p)
         return "↑" if not p else f"{', '.join(p)} lvl up ↑"
 
 
-def _draw_left_status(dd, sc, tab, before, mtl, index, is_last):
+def _draw_right_status(sc, is_last):
+    if not is_last:
+        return sc.cursor.x
+
+    cells = [
+        (BDAE_FG, 0, birthday()),
+        (DATE_FG, 0, NOW.strftime("  %d %b ")),
+        (CLOCK_FG, 0, NOW.strftime(" %H:%M  ")),
+    ]
+
+    right_status_length = sum([len(x) for _, _, x in cells])
+    draw_spaces = sc.columns - sc.cursor.x - right_status_length
+
+    if draw_spaces > 0:
+        sc.draw(" " * draw_spaces)
+
+    for fg, bg, cell in cells:
+        sc.cursor.fg, sc.cursor.bg = fg, bg
+        sc.draw(cell)
+
+    sc.cursor.fg, sc.cursor.bg = 0, 0
+    sc.cursor.x = max(sc.cursor.x, sc.columns - right_status_length)
+    return sc.cursor.x
+
+
+def _draw_left_status(dd, sc, tab, b, mtl, idx, is_last) -> int:
     if dd.leading_spaces:
         sc.draw(" " * dd.leading_spaces)
 
     # draw tab title
-    draw_title(dd, sc, tab, index)
+    draw_title(dd, sc, tab, idx)
 
-    trailing_spaces = min(mtl - 1, dd.trailing_spaces)
-    mtl -= trailing_spaces
-    extra = sc.cursor.x - before - mtl
+    trailing_spaces = min(max_title_length - 1, dd.trailing_spaces)
+    max_title_length -= trailing_spaces
+    extra = sc.cursor.x - b - max_title_length
     if extra > 0:
         sc.cursor.x -= extra + 1
         sc.draw("…")
@@ -48,35 +73,7 @@ def _draw_left_status(dd, sc, tab, before, mtl, index, is_last):
     return sc.cursor.x
 
 
-def _draw_right_status(sc, is_last):
-    if not is_last:
-        return sc.cursor.x
-
-    cells = [
-        (DATE_FG, 0, NOW.strftime("  %d %b ")),
-        (CLOCK_FG, 0, NOW.strftime(" %H:%M  ")),
-    ]
-
-    right_status_length = 0
-    for _, _, cell in cells:
-        right_status_length += len(cell)
-
-    draw_spaces = sc.columns - sc.cursor.x - right_status_length
-    if draw_spaces > 0:
-        sc.draw(" " * draw_spaces)
-
-    for fg, bg, cell in cells:
-        sc.cursor.fg = fg
-        sc.cursor.bg = bg
-        sc.draw(cell)
-    sc.cursor.fg = 0
-    sc.cursor.bg = 0
-
-    sc.cursor.x = max(sc.cursor.x, sc.columns - right_status_length)
-    return sc.cursor.x
-
-
-def draw_tab(dd, sc, tab, before, mtl, index, is_last, _):
-    end = _draw_left_status(dd, sc, tab, before, mtl, index, is_last)
+def draw_tab(dd, sc, tab, b, mtl, idx, is_last, _):
+    end = _draw_left_status(dd, sc, tab, b, mtl, idx, is_last)
     _draw_right_status(sc, is_last)
     return end
