b69dea65341ed2bc6c715ef410a4ac25fdcdc6a8
nguyenvukhang <brew4k@gmail.com>
2023-11-19 20:42:21 -0500

    nvim: update plugins

--- nvim/lua/brew/core/autocmd.lua
+++ nvim/lua/brew/core/autocmd.lua
@@ -1,24 +1,23 @@
 local c = require('brew.core')
-local vnoremap, onoremap, autocmd = c.vnoremap, c.onoremap, c.autocmd
+local o, bo = vim.opt, vim.bo
 
 local dollarDollar = function()
-  onoremap('i$', ':<c-u>norm! T$vt$<cr>')
-  onoremap('a$', ':<c-u>norm! F$vf$<cr>')
-  vnoremap('i$', 'T$ot$')
-  vnoremap('a$', 'F$of$')
+  c.onoremap('i$', ':<c-u>norm! T$vt$<cr>')
+  c.onoremap('a$', ':<c-u>norm! F$vf$<cr>')
+  c.vnoremap('i$', 'T$ot$')
+  c.vnoremap('a$', 'F$of$')
 end
 
 local cfg = {
   java = function()
-    vim.opt.tabstop, vim.opt.shiftwidth = 4, 4
+    o.tabstop, o.shiftwidth = 4, 4
   end,
   latex = function()
-    vim.opt.textwidth = 70
+    o.textwidth, bo.filetype = 70, 'tex'
     dollarDollar()
   end,
   markdown = function()
-    vim.bo.filetype = 'markdown'
-    vim.opt.textwidth = 70
+    o.textwidth, bo.filetype = 70, 'markdown'
     dollarDollar()
   end,
   cpp = function() c.comment_string('// %s') end,
@@ -26,17 +25,17 @@ local cfg = {
   astro = function() c.comment_string('// %s') end,
 }
 
-autocmd { pattern = '*.tex', callback = cfg.latex }
-autocmd { pattern = { '*.swift' }, callback = cfg.swift }
-autocmd { pattern = { '*.mdx', '*.md' }, callback = cfg.markdown }
-autocmd { pattern = { '*.java' }, callback = cfg.java }
-autocmd { pattern = { '*.cpp', '*.h', '*.c', '*.cc' }, callback = cfg.cpp }
-autocmd { pattern = { '*.astro' }, callback = cfg.astro }
+c.autocmd { pattern = '*.tex', callback = cfg.latex }
+c.autocmd { pattern = { '*.swift' }, callback = cfg.swift }
+c.autocmd { pattern = { '*.mdx', '*.md' }, callback = cfg.markdown }
+c.autocmd { pattern = { '*.java' }, callback = cfg.java }
+c.autocmd { pattern = { '*.cpp', '*.h', '*.c', '*.cc' }, callback = cfg.cpp }
+c.autocmd { pattern = { '*.astro' }, callback = cfg.astro }
 
--- autocmd({
+-- c.autocmd({
 --   pattern = { '*.tex' },
 --   callback = function() vim.fn.systemlist('make') end,
 -- }, { 'BufWritePost' })
 
 -- automatically redistribute splits when vim is resized
-autocmd({ command = 'wincmd =' }, { 'VimResized' })
+c.autocmd({ command = 'wincmd =' }, { 'VimResized' })
