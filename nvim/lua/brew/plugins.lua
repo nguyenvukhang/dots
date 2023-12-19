ef25767e12500c0e2fbb8cb20c2c59d351eee833
nguyenvukhang <brew4k@gmail.com>
2023-12-18 23:06:19 -0500

    autopairs

--- nvim/lua/brew/plugins.lua
+++ nvim/lua/brew/plugins.lua
@@ -24,6 +24,16 @@ config['ThePrimeagen/harpoon'] = function()
   end, true)
 end
 
+config['windwp/nvim-autopairs'] = function()
+  local np, Rule = require('nvim-autopairs'), require('nvim-autopairs.rule')
+  np.setup { ignored_next_char = [=[[%w%%%'%[%"%.%`]]=] }
+  np.add_rules {
+    Rule('$', '$', { 'tex', 'markdown' }):with_move(
+      function(o) return o.next_char == o.char end
+    ),
+  }
+end
+
 config['nguyenvukhang/nvim-toggler'] = function()
   local nt = require('nvim-toggler')
   local inverses = {
