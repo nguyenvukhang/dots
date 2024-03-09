2f9d501174bb76ba8b37c199a236864f0ffad749
nguyenvukhang <brew4k@gmail.com>
2024-03-09 08:41:04 +0800

    move math to math.lua

--- nvim/lua/brew/autocmd.lua
+++ nvim/lua/brew/autocmd.lua
@@ -1,27 +1,7 @@
 local c = require('brew')
 local k = vim.keymap.set
 local math = require('brew.telescope.math')
-local theorem_search = math.theorem_search
 local sil = { silent = true }
-
-local minimath = function()
-  -- completely custom search only for nguyenvukhang/math
-  k('n', '<leader>pm', function() theorem_search(true, false) end)
-  k('n', '<leader>pt', function() theorem_search(false, false) end)
-  k('v', '<leader>h', function() theorem_search(false, true, 'h') end)
-  k('v', '<leader>a', function() theorem_search(false, true, 'a') end)
-
-  -- environment wrappers
-  k('n', '<leader>be', 'cc\\begin{equation*}<CR>\\end{equation*}<esc>k')
-  k('n', '<leader>ba', 'cc\\begin{align*}<CR>\\end{align*}<esc>k')
-  k('n', '<leader>bc', 'cc\\begin{cases}<CR>\\end{cases}<esc>k')
-  k('n', '<leader>bg', 'cc\\begin{gather*}<CR>\\end{gather*}<esc>k')
-  k('n', '<leader>bt', 'cc\\begin{Theorem}<CR>\\end{Theorem}<esc>k')
-
-  -- jump to next/prev mark
-  k('n', '[[', '^k?\\v^\\\\begin\\{(' .. math.marks .. ')\\}<cr>', sil)
-  k('n', ']]', '^j/\\v^\\\\begin\\{(' .. math.marks .. ')\\}<cr>', sil)
-end
 
 local dollarDollar = function()
   k('o', 'i$', ':<c-u>norm! T$vt$<cr>', sil)
@@ -39,28 +19,7 @@ local cfg = {
     vim.opt.textwidth = 80
     vim.opt.formatoptions = vim.opt.formatoptions + 't'
     vim.bo.filetype = 'tex'
-    minimath()
-    -- go to definition for my notes
-    k('n', 'gd', function()
-      vim.cmd('w')
-      local cword = vim.fn.expand('<cword>')
-      if not cword then return end
-      local cmd = "rg --vimgrep -ttex '\\\\label\\{" .. cword .. "\\}'"
-      local output = vim.fn.systemlist(cmd)
-      if vim.v.shell_error ~= 0 then return end
-      local _, _, file, lnum = output[1]:find([[(..-):(%d+)]])
-      vim.cmd('e ' .. file)
-      vim.api.nvim_win_set_cursor(0, { tonumber(lnum), 0 })
-    end)
-    k('n', 'gr', function()
-      vim.cmd('w')
-      local cword = vim.fn.expand('<cword>')
-      if not cword then return end
-      vim.cmd(
-        'vimgrep /\\v\\\\(autoref|href)\\{' .. cword .. '\\}/ *.tex **/*.tex'
-      )
-    end)
-
+    math.remaps()
     dollarDollar()
   end,
   markdown = function()
