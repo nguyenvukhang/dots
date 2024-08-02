ff5bc8fd16a0eb240e0d54810992f5ad05b001a5
nguyenvukhang <brew4k@gmail.com>
2024-08-02 21:15:33 +0800

    updates

--- nvim/lua/brew/autocmd.lua
+++ nvim/lua/brew/autocmd.lua
@@ -9,11 +9,12 @@ local dollarDollar = function()
   k('v', 'a$', 'F$of$', sil)
 end
 
+local function set_tab(n)
+  vim.opt.tabstop, vim.opt.shiftwidth = n, n
+end
+
 local cfg = {
-  java = function()
-    vim.opt.tabstop = 4
-    vim.opt.shiftwidth = 4
-  end,
+  java = function() set_tab(4) end,
   latex = function()
     vim.opt.textwidth = 80
     vim.opt.formatoptions = vim.opt.formatoptions + 't'
@@ -26,7 +27,10 @@ local cfg = {
     vim.bo.filetype = 'markdown'
     dollarDollar()
   end,
-  cpp = function() c.comment_string('// %s') end,
+  cpp = function()
+    c.comment_string('// %s')
+    set_tab(4)
+  end,
   swift = function() c.comment_string('// %s') end,
   astro = function() c.comment_string('// %s') end,
   asm = function() c.comment_string('# %s') end,
