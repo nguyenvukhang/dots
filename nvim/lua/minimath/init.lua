d0a5b0318433678e48e7d650927ad0017451c112
nguyenvukhang <brew4k@gmail.com>
2025-01-23 14:37:05 +0800

    Update minimath lua abbrev getter

--- nvim/lua/minimath/init.lua
+++ nvim/lua/minimath/init.lua
@@ -13,14 +13,24 @@ local _abbrev_cache = {}
 ---@param filename string
 ---@return string abbreviation
 local get_abbrev = function(filename)
+  -- If a result has been computed before, then just return that.
   local res = _abbrev_cache[filename]
   if res ~= nil then return res end
+
+  -- Otherwise, we start analyzing components. One-up is the topic, two-up is
+  -- the category. We take the first letter from the category, and the
+  -- abbreviation of the topic (from `gen.topics`).
   local components = vim.fn.split(filename, '/')
   if #components < 3 then return '[ • ]' end
-  local topic = components[#components - 2]
-    .. '/'
-    .. components[#components - 1]
-  local abbrev = gen.topics[topic] or '[ • ]'
+
+  local category_abbrev = string.sub(components[#components - 2], 1, 1)
+
+  local topic = components[#components - 1]
+  local topic_abbrev = gen.topics[topic] or '???'
+
+  local abbrev = '[' .. category_abbrev .. '/' .. topic_abbrev .. ']'
+
+  -- Add to cache
   _abbrev_cache[filename] = abbrev
   return abbrev
 end
