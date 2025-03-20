4a7eb44774a1cce7ec97b9150c94e1af5686d686
nguyenvukhang <brew4k@gmail.com>
2025-03-20 08:24:05 +0800

    Update alatty conf

--- @/alatty/tab_bar.py
+++ @/alatty/tab_bar.py
@@ -1,16 +1,14 @@
 # pyright: reportMissingImports=false
+# pyright: reportCallIssue=false
 
-import datetime, json, os
+from datetime import datetime, date
 from alatty.fast_data_types import get_options
 from alatty.tab_bar import as_rgb, draw_title
 from alatty.utils import color_as_int
 
 OPTS = get_options()
-CLOCK_FG = as_rgb(color_as_int(OPTS.color3))
-DATE_FG = as_rgb(color_as_int(OPTS.color5))
-BDAE_FG = as_rgb(color_as_int(OPTS.color2))
 NWEEKS_FG = as_rgb(color_as_int(OPTS.color8))
-NOW = datetime.datetime.now
+NOW = datetime.now
 
 
 WEEKS = (
@@ -33,33 +31,19 @@ WEEKS = (
     "E2",
 )
 
-BIRTHDAY_FILE = "/Users/khang/dots/personal/birthdays/db.json"
-BIRTHDAYS = {}
-if os.path.isfile(BIRTHDAY_FILE):
-    with open(BIRTHDAY_FILE, "r") as f:
-        BIRTHDAYS = json.load(f)
+
+def week(now: date):
+    WEEK_ZERO_MONDAY = date(2025, 1, 6)
+    n = int((now - WEEK_ZERO_MONDAY).days / 7) - 1
+    return WEEKS[n] if n < len(WEEKS) else None
 
 
-def birthday(now):
-    p = BIRTHDAYS.get(now.strftime("%d-%b"), None)
-    return "↑" if not p else f"{', '.join(p)} lvl up ↑"
-
-
-def week(now: datetime.datetime):
-    w0 = datetime.datetime(2024, 1, 8)
-    idx = int((now - w0).days / 7) - 1
-    return WEEKS[idx] if idx < len(WEEKS) else "<>"
-
-
-def right_cells(now: datetime.datetime):
-    b = birthday(now)
-    w = week(now)
-    return [
-        (NWEEKS_FG, w + "  "),
-        (BDAE_FG, b if b else "x"),
-        (DATE_FG, now.strftime("  %d %b ")),
-        (CLOCK_FG, now.strftime(" %H:%M  ")),
-    ]
+def right_cells(now: datetime):
+    cells = []
+    w = week(now.date())
+    if w is not None:
+        cells.append((NWEEKS_FG, w + "  "))
+    return cells
 
 
 def _draw_left_status(dd, sc, tab, before, mtl, index, is_last):
@@ -98,10 +82,8 @@ def _draw_right_status(sc, is_last):
         sc.draw(" " * s)
 
     for fg, cell in cells:
-        sc.cursor.fg, sc.cursor.bg = fg, 0
-        sc.draw(cell)
-    sc.cursor.fg, sc.cursor.bg = 0, 0
-    sc.cursor.x = max(sc.cursor.x, sc.columns - rlen)
+        sc.cursor.fg, sc.cursor.bg, _ = fg, 0, sc.draw(cell)
+    sc.cursor.fg, sc.cursor.bg, sc.cursor.x = 0, 0, max(sc.cursor.x, sc.columns - rlen)
     return sc.cursor.x
 
 
