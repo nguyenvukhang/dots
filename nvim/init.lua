77b02c5dd9b0adf4c98b34d054ccda5c6bcc0df8
nguyenvukhang <brew4k@gmail.com>
2025-05-23 13:23:31 +0800

    Update quote pairing on rust

--- nvim/init.lua
+++ nvim/init.lua
@@ -242,7 +242,7 @@ require('brew.lazy').setup {
         ),
       }
       -- Disable '' pair on LEAN files because it means "prime".
-      np.get_rules("'")[1].not_filetypes = { 'lean' }
+      np.get_rules("'")[1].not_filetypes = { 'lean', 'rust' }
     end,
   },
 }
