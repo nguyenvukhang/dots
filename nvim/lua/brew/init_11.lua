25660f27d961ad963ffe03a3627234cae3d9f572
nguyenvukhang <brew4k@gmail.com>
2025-10-14 17:56:34 +0800

    Start from scratch with 11

---
+++ nvim/lua/brew/init_11.lua
@@ -0,0 +1,255 @@
+-- 'hrsh7th/nvim-cmp'
+require('brew.lazy_11').setup {
+  'tpope/vim-surround',
+  'wuelnerdotexe/vim-astro',
+  'nvim-treesitter/playground',
+  'mfussenegger/nvim-jdtls',
+  'vimplug/nvim-colorizer.lua',
+  'hrsh7th/vim-vsnip',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  {
+    'hrsh7th/nvim-cmp',
+    config = function()
+      local cmp = require('cmp')
+      local ct = require('cmp.types')
+      cmp.setup {
+        formatting = {
+          expandable_indicator = false,
+          format = function(_, item)
+            item.menu = nil -- remove flair from completion item
+            return item
+          end,
+        },
+        mapping = {
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-l>'] = cmp.mapping.confirm { select = true },
+        },
+        sources = cmp.config.sources({
+          {
+            name = 'nvim_lsp',
+            entry_filter = function(e) -- remove `Snippet` entries
+              return ct.lsp.CompletionItemKind[e:get_kind()] ~= 'Snippet'
+            end,
+          },
+        }, {
+          { name = 'buffer' },
+          { name = 'path' },
+        }),
+        preselect = cmp.PreselectMode.None,
+        snippet = { expand = function(x) vim.fn['vsnip#anonymous'](x.body) end },
+      }
+      -- Use buffer source for `/` search
+      cmp.setup.cmdline('/', { sources = { { name = 'buffer' } } })
+    end,
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = {
+      'nvim-telescope/telescope-fzy-native.nvim',
+      'nvim-lua/plenary.nvim',
+    },
+    config = function() require('brew.telescope') end,
+  },
+  {
+    'nguyenvukhang/nvim-toggler',
+    config = function()
+      local nt = require('nvim-toggler')
+      local inverses = {
+        ['- [ ]'] = '- [x]',
+        ['shift'] = 'unshift',
+        ['next'] = 'prev',
+        ['odd'] = 'even',
+        ['forall'] = 'exists',
+        ['row'] = 'column',
+        ['positive'] = 'negative',
+        ['horizontal'] = 'vertical',
+        ['min'] = 'max',
+        ['width'] = 'height',
+        ['sin'] = 'cos',
+        ['begin'] = 'end',
+        ['True'] = 'False',
+        ['TRUE'] = 'FALSE',
+        ['cot'] = 'tan',
+        ['sec'] = 'csc',
+        ['good'] = 'bad',
+        ['ON'] = 'OFF',
+        ['Yes'] = 'No',
+        ['and'] = 'or',
+      }
+      nt.setup {
+        inverses = inverses,
+        remove_default_keybinds = true,
+        autoselect_longest_match = true,
+      }
+      vim.keymap.set({ 'n', 'v' }, '<leader>i', nt.toggle, { silent = true })
+    end,
+  },
+  {
+    'neovim/nvim-lspconfig',
+    tag = 'v1.8.0',
+    config = function()
+      local lsp = require('brew.lsp')
+      lsp.rust()
+      lsp.lean()
+      lsp.clangd()
+      lsp.zls()
+      lsp.lua()
+      lsp.typescript()
+      lsp.python()
+      lsp.swift()
+      lsp.ocamllsp()
+      -- lsp.matlab_ls()
+      -- lsp.astro()
+      lsp.go()
+    end,
+  },
+  {
+    'stevearc/conform.nvim',
+    tag = 'v7.1.0',
+    config = function()
+      local conform = require('conform')
+      conform.formatters.rustfmt = { options = { default_edition = '2024' } }
+      conform.setup {
+        notify_on_error = true,
+        formatters = {
+          latexindent = function()
+            -- local lnum = tonumber(vim.fn.line('.'))
+            -- local t, b = lnum - 50, lnum + 50
+            -- local lines = '--lines ' .. t .. '-' .. b
+            return { prepend_args = { '-l', '-g=/dev/null' } }
+            -- return { prepend_args = { '-l' } }
+          end,
+          tex_fmt = function()
+            return { command = 'tex-fmt', args = { '--stdin' } }
+          end,
+          java_custom = function()
+            local cwd = vim.fn.getcwd()
+            local has = function(x) return string.find(cwd, x) end
+            if has('repos/tp') and has('/src/') and has('/java/') then
+              local gradle = vim.fs.find('gradlew', { upward = true })[1]
+              local root = vim.fs.dirname(gradle)
+              -- vim.cmd('!cd ' .. root .. '; ./gradlew spotlessApply')
+              -- print("Formatted with gradle spotless!")
+              return nil
+            end
+            return { command = 'clang-format' }
+          end,
+        },
+        async = true,
+        formatters_by_ft = {
+          java = { 'java_custom' },
+          zig = { 'zigfmt' },
+          markdown = { 'prettier' },
+          css = { 'prettier' },
+          astro = { 'prettier' },
+          c = { 'clang_format' },
+          cpp = { 'clang_format' },
+          bash = { 'shfmt' },
+          sh = { 'shfmt' },
+          zsh = { 'shfmt' },
+          lua = { 'stylua' },
+          python = { 'black' },
+          go = { 'gofmt' },
+          ocaml = { 'ocamlformat' },
+          javascript = { 'prettier' },
+          json = { 'prettier' },
+          yaml = { 'prettier' },
+          tex = { 'latexindent' },
+          rust = { 'rustfmt' },
+          swift = { 'swiftformat' },
+          typescriptreact = { 'prettier' },
+        },
+      }
+    end,
+  },
+  {
+    'terrortylor/nvim-comment',
+    keys = {
+      { '<C-c>', ':CommentToggle<CR>' },
+      { '<C-c>', ':CommentToggle<CR>', mode = 'v' },
+    },
+    config = function()
+      require('nvim_comment').setup { create_mappings = false }
+    end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    config = function()
+      local languages = { 'latex' }
+      -- javascript, typescript, c, lua, rust, tsx, css, astro, java,
+      -- latex, markdown, markdown_inline, python, swift
+      require('nvim-treesitter.configs').setup {
+        highlight = {
+          enable = true,
+          disable = function(l) return not vim.tbl_contains(languages, l) end,
+          additional_vim_regex_highlighting = false,
+        },
+        ensure_installed = languages,
+      }
+      -- local p_config = require("nvim-treesitter.parsers").get_parser_config()
+      -- print(vim.inspect(require("nvim-treesitter.parsers")))
+    end,
+  },
+  {
+    'nguyenvukhang/lean.nvim',
+    tag = 'nvim-0.9',
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'nvim-lua/plenary.nvim',
+    },
+    config = function()
+      local lean = require('lean')
+      local base = require('brew.lsp').base
+      lean.setup {
+        lsp = base {
+          init_options = {
+            editDelay = 100000,
+          },
+        },
+        infoview = {
+          autoopen = false,
+        },
+        progress_bars = { enable = false },
+        goal_markers = {
+          accomplished = '',
+          unsolved = '',
+        },
+      }
+      vim.keymap.set(
+        'n',
+        '<leader>u',
+        ':LeanInfoviewToggle<cr>',
+        { silent = true }
+      )
+    end,
+  },
+  {
+    'windwp/nvim-autopairs',
+    config = function()
+      local np, Rule = require('nvim-autopairs'), require('nvim-autopairs.rule')
+      np.setup()
+      np.add_rules {
+        Rule('$', '$', { 'tex', 'markdown' }):with_move(
+          function(o) return o.next_char == o.char end
+        ),
+      }
+      -- Disable '' pair on LEAN files because it means "prime".
+      np.get_rules("'")[1].not_filetypes = { 'lean', 'rust' }
+    end,
+  },
+}
+
+-- independent of plugins, server-friendly
+require('brew.sets')
+require('brew.remaps')
+require('brew.commands')
+require('brew.statusline')
+require('brew.autocmd')
+require('harpoon').my_setup()
+
+vim.cmd('colo gruvbox8')
+-- require('gruvbox').load()
---
+++ nvim/lua/brew/lazy_11.lua
@@ -0,0 +1,32 @@
+local M = {}
+
+---@param lazypath string
+-- Create a user command `:Boot` to quickly install lazy.nvim
+local function prime_bootstrap(lazypath)
+  vim.notify('lazy.nvim not installed. Run `:Boot` to install it.')
+  vim.api.nvim_create_user_command('Boot', function()
+    vim.notify('installing lazy...')
+    vim.fn.system {
+      'git',
+      'clone',
+      '--filter=blob:none',
+      '--single-branch',
+      'https://github.com/folke/lazy.nvim.git',
+      lazypath,
+    }
+    vim.notify('done installing lazy.nvim')
+  end, {})
+end
+
+-- load plugins using lazy.nvim
+---@param plugins table list of plugins to pass straight to lazy.nvim
+M.setup = function(plugins)
+  local root = vim.fn.stdpath('data') .. '/lazy11'
+  local lp = root .. '/lazy.nvim'
+  if not vim.loop.fs_stat(lp) then return prime_bootstrap(lp) end
+  vim.opt.runtimepath:prepend(lp)
+  vim.g.mapleader = ' '
+  require('lazy').setup(plugins)
+end
+
+return M
