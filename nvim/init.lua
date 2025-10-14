d86c23acd464a897ef323001ba66d2a940f65c00
nguyenvukhang <brew4k@gmail.com>
2025-10-14 17:59:57 +0800

    From scratch

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,94 +1,94 @@
 -- 'hrsh7th/nvim-cmp'
 require('brew.lazy').setup {
   'nvim-lua/plenary.nvim',
-  -- 'tpope/vim-surround',
+  'tpope/vim-surround',
   -- 'wuelnerdotexe/vim-astro',
   -- 'nvim-treesitter/playground',
   -- 'mfussenegger/nvim-jdtls',
-  -- 'vimplug/nvim-colorizer.lua',
-  -- 'hrsh7th/vim-vsnip',
-  -- 'hrsh7th/cmp-nvim-lsp',
-  -- 'hrsh7th/cmp-buffer',
-  -- 'hrsh7th/cmp-path',
-  -- 'hrsh7th/cmp-cmdline',
-  -- {
-  --   'hrsh7th/nvim-cmp',
-  --   config = function()
-  --     local cmp = require('cmp')
-  --     local ct = require('cmp.types')
-  --     cmp.setup {
-  --       formatting = {
-  --         expandable_indicator = false,
-  --         format = function(_, item)
-  --           item.menu = nil -- remove flair from completion item
-  --           return item
-  --         end,
-  --       },
-  --       mapping = {
-  --         ['<C-n>'] = cmp.mapping.select_next_item(),
-  --         ['<C-p>'] = cmp.mapping.select_prev_item(),
-  --         ['<C-l>'] = cmp.mapping.confirm { select = true },
-  --       },
-  --       sources = cmp.config.sources({
-  --         {
-  --           name = 'nvim_lsp',
-  --           entry_filter = function(e) -- remove `Snippet` entries
-  --             return ct.lsp.CompletionItemKind[e:get_kind()] ~= 'Snippet'
-  --           end,
-  --         },
-  --       }, {
-  --         { name = 'buffer' },
-  --         { name = 'path' },
-  --       }),
-  --       preselect = cmp.PreselectMode.None,
-  --       snippet = { expand = function(x) vim.fn['vsnip#anonymous'](x.body) end },
-  --     }
-  --     -- Use buffer source for `/` search
-  --     cmp.setup.cmdline('/', { sources = { { name = 'buffer' } } })
-  --   end,
-  -- },
-  -- {
-  --   'nvim-telescope/telescope.nvim',
-  --   dependencies = {
-  --     'nvim-telescope/telescope-fzy-native.nvim',
-  --     'nvim-lua/plenary.nvim',
-  --   },
-  --   config = function() require('brew.telescope') end,
-  -- },
-  -- {
-  --   'nguyenvukhang/nvim-toggler',
-  --   config = function()
-  --     local nt = require('nvim-toggler')
-  --     local inverses = {
-  --       ['- [ ]'] = '- [x]',
-  --       ['shift'] = 'unshift',
-  --       ['next'] = 'prev',
-  --       ['odd'] = 'even',
-  --       ['forall'] = 'exists',
-  --       ['row'] = 'column',
-  --       ['positive'] = 'negative',
-  --       ['horizontal'] = 'vertical',
-  --       ['min'] = 'max',
-  --       ['width'] = 'height',
-  --       ['sin'] = 'cos',
-  --       ['begin'] = 'end',
-  --       ['True'] = 'False',
-  --       ['TRUE'] = 'FALSE',
-  --       ['cot'] = 'tan',
-  --       ['sec'] = 'csc',
-  --       ['good'] = 'bad',
-  --       ['ON'] = 'OFF',
-  --       ['Yes'] = 'No',
-  --       ['and'] = 'or',
-  --     }
-  --     nt.setup {
-  --       inverses = inverses,
-  --       remove_default_keybinds = true,
-  --       autoselect_longest_match = true,
-  --     }
-  --     vim.keymap.set({ 'n', 'v' }, '<leader>i', nt.toggle, { silent = true })
-  --   end,
-  -- },
+  'vimplug/nvim-colorizer.lua',
+  'hrsh7th/vim-vsnip',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  {
+    'hrsh7th/nvim-cmp',
+    config = function()
+      local cmp = require('cmp')
+      local ct = require('cmp.types')
+      cmp.setup {
+        formatting = {
+          expandable_indicator = false,
+          format = function(_, item)
+            item.menu = nil -- remove flair from completion item
+            return item
+          end,
+        },
+        mapping = {
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-l>'] = cmp.mapping.confirm { select = true },
+        },
+        sources = cmp.config.sources({
+          {
+            name = 'nvim_lsp',
+            entry_filter = function(e) -- remove `Snippet` entries
+              return ct.lsp.CompletionItemKind[e:get_kind()] ~= 'Snippet'
+            end,
+          },
+        }, {
+          { name = 'buffer' },
+          { name = 'path' },
+        }),
+        preselect = cmp.PreselectMode.None,
+        snippet = { expand = function(x) vim.fn['vsnip#anonymous'](x.body) end },
+      }
+      -- Use buffer source for `/` search
+      cmp.setup.cmdline('/', { sources = { { name = 'buffer' } } })
+    end,
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = {
+      'nvim-telescope/telescope-fzy-native.nvim',
+      'nvim-lua/plenary.nvim',
+    },
+    config = function() require('brew.telescope') end,
+  },
+  {
+    'nguyenvukhang/nvim-toggler',
+    config = function()
+      local nt = require('nvim-toggler')
+      local inverses = {
+        ['- [ ]'] = '- [x]',
+        ['shift'] = 'unshift',
+        ['next'] = 'prev',
+        ['odd'] = 'even',
+        ['forall'] = 'exists',
+        ['row'] = 'column',
+        ['positive'] = 'negative',
+        ['horizontal'] = 'vertical',
+        ['min'] = 'max',
+        ['width'] = 'height',
+        ['sin'] = 'cos',
+        ['begin'] = 'end',
+        ['True'] = 'False',
+        ['TRUE'] = 'FALSE',
+        ['cot'] = 'tan',
+        ['sec'] = 'csc',
+        ['good'] = 'bad',
+        ['ON'] = 'OFF',
+        ['Yes'] = 'No',
+        ['and'] = 'or',
+      }
+      nt.setup {
+        inverses = inverses,
+        remove_default_keybinds = true,
+        autoselect_longest_match = true,
+      }
+      vim.keymap.set({ 'n', 'v' }, '<leader>i', nt.toggle, { silent = true })
+    end,
+  },
   -- {
   --   'neovim/nvim-lspconfig',
   --   tag = 'v1.8.0',
@@ -108,75 +108,74 @@ require('brew.lazy').setup {
   --     lsp.go()
   --   end,
   -- },
-  -- {
-  --   'stevearc/conform.nvim',
-  --   tag = 'v7.1.0',
-  --   config = function()
-  --     local conform = require('conform')
-  --     conform.formatters.rustfmt = { options = { default_edition = '2024' } }
-  --     conform.setup {
-  --       notify_on_error = true,
-  --       formatters = {
-  --         latexindent = function()
-  --           -- local lnum = tonumber(vim.fn.line('.'))
-  --           -- local t, b = lnum - 50, lnum + 50
-  --           -- local lines = '--lines ' .. t .. '-' .. b
-  --           return { prepend_args = { '-l', '-g=/dev/null' } }
-  --           -- return { prepend_args = { '-l' } }
-  --         end,
-  --         tex_fmt = function()
-  --           return { command = 'tex-fmt', args = { '--stdin' } }
-  --         end,
-  --         java_custom = function()
-  --           local cwd = vim.fn.getcwd()
-  --           local has = function(x) return string.find(cwd, x) end
-  --           if has('repos/tp') and has('/src/') and has('/java/') then
-  --             local gradle = vim.fs.find('gradlew', { upward = true })[1]
-  --             local root = vim.fs.dirname(gradle)
-  --             -- vim.cmd('!cd ' .. root .. '; ./gradlew spotlessApply')
-  --             -- print("Formatted with gradle spotless!")
-  --             return nil
-  --           end
-  --           return { command = 'clang-format' }
-  --         end,
-  --       },
-  --       async = true,
-  --       formatters_by_ft = {
-  --         java = { 'java_custom' },
-  --         zig = { 'zigfmt' },
-  --         markdown = { 'prettier' },
-  --         css = { 'prettier' },
-  --         astro = { 'prettier' },
-  --         c = { 'clang_format' },
-  --         cpp = { 'clang_format' },
-  --         bash = { 'shfmt' },
-  --         sh = { 'shfmt' },
-  --         zsh = { 'shfmt' },
-  --         lua = { 'stylua' },
-  --         python = { 'black' },
-  --         go = { 'gofmt' },
-  --         ocaml = { 'ocamlformat' },
-  --         javascript = { 'prettier' },
-  --         json = { 'prettier' },
-  --         yaml = { 'prettier' },
-  --         tex = { 'latexindent' },
-  --         rust = { 'rustfmt' },
-  --         swift = { 'swiftformat' },
-  --         typescriptreact = { 'prettier' },
-  --       },
-  --     }
-  --   end,
-  -- },
-  -- {
-  --   'terrortylor/nvim-comment',
-  --   keys = {
-  --     { '<C-c>', ':CommentToggle<CR>' },
-  --     { '<C-c>', ':CommentToggle<CR>', mode = 'v' },
-  --   },
-  --   config = function()
-  --     require('nvim_comment').setup { create_mappings = false }
-  --   end,
-  -- },
+  {
+    'stevearc/conform.nvim',
+    config = function()
+      local conform = require('conform')
+      conform.setup {
+        notify_on_error = true,
+        formatters = {
+          rustfmt = { options = { default_edition = '2024' } },
+          latexindent = function()
+            -- local lnum = tonumber(vim.fn.line('.'))
+            -- local t, b = lnum - 50, lnum + 50
+            -- local lines = '--lines ' .. t .. '-' .. b
+            return { prepend_args = { '-l', '-g=/dev/null' } }
+            -- return { prepend_args = { '-l' } }
+          end,
+          tex_fmt = function()
+            return { command = 'tex-fmt', args = { '--stdin' } }
+          end,
+          java_custom = function()
+            local cwd = vim.fn.getcwd()
+            local has = function(x) return string.find(cwd, x) end
+            if has('repos/tp') and has('/src/') and has('/java/') then
+              local gradle = vim.fs.find('gradlew', { upward = true })[1]
+              local root = vim.fs.dirname(gradle)
+              -- vim.cmd('!cd ' .. root .. '; ./gradlew spotlessApply')
+              -- print("Formatted with gradle spotless!")
+              return nil
+            end
+            return { command = 'clang-format' }
+          end,
+        },
+        async = true,
+        formatters_by_ft = {
+          java = { 'java_custom' },
+          zig = { 'zigfmt' },
+          markdown = { 'prettier' },
+          css = { 'prettier' },
+          astro = { 'prettier' },
+          c = { 'clang_format' },
+          cpp = { 'clang_format' },
+          bash = { 'shfmt' },
+          sh = { 'shfmt' },
+          zsh = { 'shfmt' },
+          lua = { 'stylua' },
+          python = { 'black' },
+          go = { 'gofmt' },
+          ocaml = { 'ocamlformat' },
+          javascript = { 'prettier' },
+          json = { 'prettier' },
+          yaml = { 'prettier' },
+          tex = { 'latexindent' },
+          rust = { 'rustfmt' },
+          swift = { 'swiftformat' },
+          typescriptreact = { 'prettier' },
+        },
+      }
+    end,
+  },
+  {
+    'terrortylor/nvim-comment',
+    keys = {
+      { '<C-c>', ':CommentToggle<CR>' },
+      { '<C-c>', ':CommentToggle<CR>', mode = 'v' },
+    },
+    config = function()
+      require('nvim_comment').setup { create_mappings = false }
+    end,
+  },
   -- {
   --   'nvim-treesitter/nvim-treesitter',
   --   config = function()
@@ -246,7 +245,7 @@ require('brew.lazy').setup {
 
 -- independent of plugins, server-friendly
 require('brew.sets')
--- require('brew.remaps')
+require('brew.remaps')
 -- require('brew.commands')
 -- require('brew.statusline')
 -- require('brew.autocmd')
